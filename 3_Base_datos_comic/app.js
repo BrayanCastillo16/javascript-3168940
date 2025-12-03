84d39c6469f93ec0dba1481f95e463c739b8f9cf
BrayanCastillo16 <brayancastillosepulveda16@gmail.com>
2025-12-03 15:27:57 -0500

    Add files via upload

--- 3_Base_datos_comic/app.js
+++ 3_Base_datos_comic/app.js
@@ -1,3 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.querySelectorAll('.carousel-slide');
+    const prevButton = document.querySelector('.prev-button');
+    const nextButton = document.querySelector('.next-button');
+    let currentSlide = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.display = (i === index) ? 'block' : 'none';
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    function prevSlide() {
+        currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+        showSlide(currentSlide);
+    }
+
+    prevButton.addEventListener('click', prevSlide);
+    nextButton.addEventListener('click', nextSlide);
+
+    // Muestra la primera diapositiva al cargar
+    showSlide(currentSlide);
+});
+
+
 import { comic } from "./bd.js";
 
       const Numeropersonajes = document.querySelector(".personajes");
@@ -9,12 +39,12 @@ import { comic } from "./bd.js";
         section.classList.add("section");
         section.innerHTML = `
        <h2>${char.nombre}</h2>
-  <div class="cards">
-    <div class="card">
-      <img src="${char.imagen}" alt="Juanes">
-      <p>${char.descripcion}</p>
-      <h3>Juanes</h3>
-    </div>
+       <div class="cards">
+       <div class="card">
+       <img src="${char.imagen}" alt="Juanes">
+       <p>${char.descripcion}</p>
+       <h3>Juanes</h3>
+       </div>
         `;
 
         Numeropersonajes.appendChild(section);
@@ -32,7 +62,7 @@ import { comic } from "./bd.js";
         section.classList.add("section");
         section.innerHTML = `
 
-          <h1>Capítulo 1 - El descubrimiento</h1>
+          <h1></h1>
          <div class="Hero-slides" id="heroSlides">
          <.div class="slide" data-index="0">
          <div class="slide-content">
@@ -41,7 +71,7 @@ import { comic } from "./bd.js";
          <div class="hero_controls">
           <h3>Capítulo 1</h3>
           <p>El descubrimiento</p>
-        </div>
+         </div>
 
 
             <a class="btn" href="capitulos.html">Ver Mas</a>
